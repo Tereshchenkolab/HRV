8f71d94bdd6106e4ff0580413c9c4a7adfb38a50
muammarkabir <50457105+muammarkabir@users.noreply.github.com>
2019-05-09 11:49:24 -0400

    Update spectral_analysis_HRV.m
    Provided explanations/comments and kept only necessary sections as used in the paper

--- HRV_full/spectral_analysis_HRV.m
+++ HRV_full/spectral_analysis_HRV.m
@@ -1,4 +1,4 @@
-function [LF,HF,VLF,TP,LFHFratio] = spectral_analysis_HRV(RR,Fs,type)
+function [LF,HF,LFHFratio] = spectral_analysis_HRV(RR,Fs,type)
 %spectral_analysis_HRV Spectral analysis of HRV.
 %   The function uses FFT to compute the spectral density function of the
 %   interpolated RR tachogram.
@@ -6,14 +6,11 @@ function [LF,HF,VLF,TP,LFHFratio] = spectral_analysis_HRV(RR,Fs,type)
 % Input:
 %   RR: RR intervals in seconds.
 %   Fs: Sampling frequency.
-%   type: interpolation type. Look up interp1 function of Matlab for
-%   accepted types (default: 'spline').
+%   type: interpolation type (default: 'spline') 
 %
 % Output:
 %   LF: Low frequency Power
 %   HF: High frequency Power
-%   VLF: Very low frequency Power
-%   TP: Total Power
 %   LFHFratio: LF/HF
      
     if nargin<2 || isempty(Fs)
@@ -24,42 +21,37 @@ function [LF,HF,VLF,TP,LFHFratio] = spectral_analysis_HRV(RR,Fs,type)
         type = 'spline';
     end
     
-    RR = RR(:);
+    RR = RR(:); %Convert to column vector
     
     switch type
         case 'none'
             RR_rsmp = RR;
         otherwise
             if sum(isnan(RR))==0 && length(RR)>1
-                ANN = cumsum(RR)-RR(1);
-                % use interp1 methods for resampling
-                RR_rsmp = interp1(ANN,RR,0:1/Fs:ANN(end),type);
+                ANN = cumsum(RR)-RR(1);                         % regenerating time-points
+                RR_rsmp = interp1(ANN,RR,0:1/Fs:ANN(end),type); % resampling
             else
                 RR_rsmp = [];
             end
     end
     
     % FFT
+    % Find length
     L = length(RR_rsmp); 
     
     if L==0 
         LFHFratio = NaN;
-        VLF = NaN;
         LF = NaN;
         HF = NaN;
-        TP = NaN;
     else
-        NFFT = 2^nextpow2(L);
+        NFFT = 2^nextpow2(L);             % number of samples for performing fft
         Y = fft(zscore(RR_rsmp),NFFT)/L;
-        %Y = fft(nanzscore(RR_rsmp),NFFT)/L;
-        f = Fs/2*linspace(0,1,NFFT/2+1);  
-
+        f = Fs/2*linspace(0,1,NFFT/2+1);  % Consider only first half of frequency spectrum 
+                                          % as second half is the mirror of 1st half  
         YY = 2*abs(Y(1:NFFT/2+1));
 
-        VLF = sum(YY(f<=0.04));
-        LF = sum(YY(f<=0.15))-VLF;  
-        HF = sum(YY(f<=0.4))-VLF-LF;
-        TP = sum(YY(f<=0.4));
+        LF = sum(YY(f>0.04 & f<=0.15));  
+        HF = sum(YY(f>0.15 & f<=0.4));
   
         LFHFratio = LF/HF; 
     end
